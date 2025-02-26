a06e4580d44478bf7d9ead30020a736d5bda1a5c
Gaspare Monte <Gas9220@users.noreply.github.com>
2025-02-26 10:14:05 +0100

    create searchBar and tableView

---
+++ StoreSearch/Models/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Gaspare Monte on 26/02/25.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
