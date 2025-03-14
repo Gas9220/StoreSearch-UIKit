04674bed9930e965ef26719ea6f15d55f234e687
Gaspare Monte <Gas9220@users.noreply.github.com>
2025-03-14 10:18:02 +0100

    create custom table cells

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,25 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Gaspare Monte on 14/03/25.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(named: "SearchBar")?.withAlphaComponent(0.5)
+        selectedBackgroundView = selectedView
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- StoreSearch/ViewControllers/SearchViewController.swift
+++ StoreSearch/ViewControllers/SearchViewController.swift
@@ -16,7 +16,14 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.becomeFirstResponder()
         tableView.contentInset = UIEdgeInsets(top: 56, left: 0, bottom: 0, right: 0)
+        
+        var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
+        cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
+
     }
 }
 
@@ -33,21 +40,22 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "SearchResultCell"
-        
-        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
         if searchResults.count == 0 {
-            cell!.textLabel!.text = "(Nothing found)"
-            cell!.detailTextLabel!.text = ""
+            return tableView.dequeueReusableCell(
+                withIdentifier: TableView.CellIdentifiers.nothingFoundCell,
+                for: indexPath
+            )
         } else {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TableView.CellIdentifiers.searchResultCell,
+                for: indexPath
+            ) as! SearchResultCell
+            
             let searchResult = searchResults[indexPath.row]
-            cell!.textLabel!.text = searchResult.name
-            cell!.detailTextLabel!.text = searchResult.artistName
+            cell.nameLabel.text = searchResult.name
+            cell.artistNameLabel.text = searchResult.artistName
+            return cell
         }
-        return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -83,5 +91,12 @@ extension SearchViewController: UISearchBarDelegate {
     
     func position(for bar: UIBarPositioning) -> UIBarPosition {
         return .topAttached
+    }
+}
+
+struct TableView {
+    struct CellIdentifiers {
+        static let searchResultCell = "SearchResultCell"
+        static let nothingFoundCell = "NothingFoundCell"
     }
 }
